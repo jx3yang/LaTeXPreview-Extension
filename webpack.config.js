0282373e4f61c485e74f22b0de245aa3a206a389
jx3yang <65411020+jx3yang@users.noreply.github.com>
2021-09-04 02:47:14 -0400

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const { resolve } = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+const tsRule = {
+  test: /\.ts(x?)$/,
+  exclude: /node_modules/,
+  use: 'ts-loader',
+}
+
+const plugins = [
+  new HTMLWebpackPlugin({
+    template: './src/pages/Popup/index.html',
+    filename: 'Popup.html',
+    chunks: ['Popup'],
+  }),
+  new HTMLWebpackPlugin({
+    template: './src/pages/Options/index.html',
+    filename: 'Options.html',
+    chunks: ['Options'],
+  }),
+  new CopyWebpackPlugin({
+    patterns: [
+      { from: 'public', to: '.' },
+    ],
+  }),
+  new CleanWebpackPlugin(),
+]
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    Popup: './src/pages/Popup/index.tsx',
+    Options: './src/pages/Options/index.tsx',
+    content: './src/scripts/content.ts',
+    background: './src/scripts/background.ts',
+  },
+  output: {
+    filename: '[name].js',
+    path: resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [tsRule],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    alias: {
+      "@": resolve(__dirname, 'src/'),
+    }
+  },
+  plugins,
+}
